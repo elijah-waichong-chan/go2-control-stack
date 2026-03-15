527636eb5c294b01b4b692c3d78b228b2477ef97
Elijah Chan <chanwaichong0352@gmail.com>
2026-03-15 01:46:42 -0700

    removed old ik files

--- ros2_ws/src/core/arm_controller/arm_controller/__init__.py
+++ ros2_ws/src/core/arm_controller/arm_controller/__init__.py
@@ -1,17 +1,1 @@
-from .d1_ik import (
-    DEFAULT_D1_ARM_JOINT_NAMES,
-    DEFAULT_END_EFFECTOR_FRAME,
-    D1ArmIKSolver,
-    IKSolveError,
-    resolve_d1_urdf_path,
-    solve_d1_position_ik,
-)
-
-__all__ = [
-    "DEFAULT_D1_ARM_JOINT_NAMES",
-    "DEFAULT_END_EFFECTOR_FRAME",
-    "D1ArmIKSolver",
-    "IKSolveError",
-    "resolve_d1_urdf_path",
-    "solve_d1_position_ik",
-]
+"""arm_controller package."""
